cb7ff2fe581fb062f1c352bb1a6fde0588dca4af
Andrew Duncan <arduncan@bigpond.com>
2012-01-12 16:42:13 +1100

    Added GPL2

--- Examples/LcdColumnGraph/LcdColumnGraph.ino
+++ Examples/LcdColumnGraph/LcdColumnGraph.ino
@@ -1,6 +1,18 @@
 #include <LcdColumnGraph.h>
 #include <LiquidCrystal.h>
 
+//-------------------------------------------------------------------------
+// Copyright (C) 2012 Andrew Duncan.
+//
+// This software may be distributed and modified under the terms of the GNU
+// General Public License version 2 (GPL2) as published by the Free Software
+// Foundation and appearing in the file GPL2.TXT included in the packaging
+// of this file. Please note that GPL2 Section 2[b] requires that all works
+// based on this software must also be made publicly available under the
+// terms of the GPL2 ("Copyleft").
+//
+//-------------------------------------------------------------------------
+
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 LcdColumnGraph lcdColumnGraph(lcd, -8, 8, 0);
 
